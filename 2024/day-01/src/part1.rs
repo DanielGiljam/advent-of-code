cd2f9fdb373f65f84352121b3837dc0f34c139ad
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-03 08:36:54 +0200

    do day-01 part1 and part2

--- 2024/day-01/src/part1.rs
+++ 2024/day-01/src/part1.rs
@@ -1,6 +1,19 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut left = vec![];
+    let mut right = vec![];
+    for line in input.lines() {
+        let mut lids = line.split_whitespace();
+        left.push(lids.next().unwrap().parse::<i32>().unwrap());
+        right.push(lids.next().unwrap().parse::<i32>().unwrap());
+    }
+    left.sort();
+    right.sort();
+    let total_distance: i32 = std::iter::zip(left, right)
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+    dbg!(total_distance);
+    return Ok(total_distance.to_string());
 }
 
 #[cfg(test)]
@@ -9,9 +22,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+                           4   3
+                           2   5
+                           1   3
+                           3   9
+                           3   3";
+        assert_eq!("11", process(input)?);
         Ok(())
     }
 }
--- 2024/day-01/src/part2.rs
+++ 2024/day-01/src/part2.rs
@@ -1,6 +1,18 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut left = vec![];
+    let mut right = vec![];
+    for line in input.lines() {
+        let mut lids = line.split_whitespace();
+        left.push(lids.next().unwrap().parse::<usize>().unwrap());
+        right.push(lids.next().unwrap().parse::<usize>().unwrap());
+    }
+    let similarity_score: usize = left
+        .iter()
+        .map(|l| l * right.iter().filter(|r| r == &l).count())
+        .sum();
+    dbg!(similarity_score);
+    return Ok(similarity_score.to_string());
 }
 
 #[cfg(test)]
@@ -9,9 +21,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+                           4   3
+                           2   5
+                           1   3
+                           3   9
+                           3   3";
+        assert_eq!("31", process(input)?);
         Ok(())
     }
 }
